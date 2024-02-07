36f423f728fa3b78773d20a512ea86fdfe500ee3
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-07 18:18:04 +0100

    Implement details component

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,19 @@
       <img src="./assets/pinia-logo.svg" alt="pinia-logo">
       <h1>Pinia Tasks</h1>
     </header>
+    <div class="task-list">
+        <div v-for="task in taskStore.tasks">
+          <TaskDetails :task="task" />
+        </div>
+    </div>
   </main>
 </template>
 
-<script>
-  export default {
-    
-  }
+<script setup>
+  import TaskDetails from './components/TaskDetails.vue'
+  import { useTaskStore } from './stores/TaskStore'
+
+  const taskStore = useTaskStore()
+  console.log(taskStore)
 </script>
 
