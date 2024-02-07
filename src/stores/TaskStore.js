bbc0ea0a38ae6c91d8b67b0c2b4d3a3fa14038b6
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-07 18:01:34 +0100

    Install pinia and setup store

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+
+export const useTaskStore = defineStore('taskStore', {
+  state: () => ({
+    tasks: [
+      { id: 1, title: 'Buy milk', isFav: false },
+      { id: 2, title: 'Learn pinia', isFav: true }
+    ]
+  })
+})
